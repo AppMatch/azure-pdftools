9acdadb6534f1fc55ccaa5708333967f1a6a4870
Jernej <j@eee.si>
2019-01-18 12:15:17 +0100

    cleanup

--- test/index.test.js
+++ test/index.test.js
@@ -15,20 +15,21 @@ describe('#mutool', () => {
     });
   });
 
-  it('should generate an png for given page', (done) => {
+  it('should generate a png image for given page', (done) => {
 
     const startPage = 1;
     const endPage = 1;
-    const filePath = `${__dirname}\\pdf\\SundanceTermite.pdf`;
-    const outputFile = `${__dirname}\\png\\x-p${startPage}--%d.png`;
+    const filePath = `${__dirname}\\pdf\\cupertino_usd.pdf`;
+    const outputFile = `${__dirname}\\pdf\\x-p${startPage}--%d.png`;
     const resolution = 288;
 
-    const Mutool = mutool(['draw', `-s -r ${resolution} -c gray -o ${outputFile}`, filePath, `${startPage}-${endPage}`]);
+    const Mutool = mutool(['draw', `-r ${resolution} -c gray -F png -o ${outputFile}`, filePath, `${startPage}-${endPage}`]);
 
     Mutool.exec((err) => {
+      console.error("png generate exec err", err)
       if (err) return done(err);
 
       done();
     });
-  })
+  });
 });
